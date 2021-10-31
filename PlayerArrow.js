ba6463e06980dc6e8a7b75b3651e03470750dc52
InfinitiFalcon <88100491+InfinitiFalcon@users.noreply.github.com>
2021-10-31 11:49:49 -0400

    Add files via upload

--- PlayerArrow.js
+++ PlayerArrow.js
@@ -7,14 +7,25 @@ class PlayerArrow {
     this.width = width;
     this.height = height;
     this.body = Bodies.rectangle(x, y, this.width, this.height, options);
-    this.image = loadImage("./assets/arrow.png");
+    this.image = loadImage("arrow.png");
     this.archerAngle = archerAngle;
     this.velocity = 0;
     World.add(world, this.body);
   }
+  
+  //create a shoot function
+ 
 
   display() {
   
-   
+    var pos = this.body.position;
+    var angle = this.body.angle;
+
+    push();
+    translate(pos.x, pos.y);
+    rotate(angle);
+    imageMode(CENTER);
+    image(this.image, 0, 0, this.width, this.height);
+    pop();
   }
 }
